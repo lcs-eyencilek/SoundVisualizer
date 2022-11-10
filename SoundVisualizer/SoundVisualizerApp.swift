abbc4ac743d907ca6b3ba3f1d9a5a048524dfb5a
lcs-eyencilek <90844648+lcs-eyencilek@users.noreply.github.com>
2022-11-09 19:46:28 -0500

    Initial Commit

---
+++ SoundVisualizer/SoundVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoundVisualizerApp.swift
+//  SoundVisualizer
+//
+//  Created by Efe Yencilek on 2022-11-09.
+//
+
+import SwiftUI
+
+@main
+struct SoundVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
